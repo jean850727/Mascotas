d46ab2d0d70cdc0fcfa44f773a4fdbed161e5143
Jeannette Saldaña Bolfeta <k.h.cj.850727@hotmail.com>
2024-07-01 20:07:09 -0600

    actualizacion

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
 import Page404 from "./pages/page404";
 import Header from "./components/Header"
+import Post from './pages/post';
+import Categoria from './pages/Categoria';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
@@ -16,6 +18,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sobre' element={<Sobre />} />
+        <Route path='/posts/:id' element={<Post />} />
+        <Route path='/categoria/:id' element={<Categoria />}/>
         <Route path='*' element={<Page404 />} />
       </Routes>
 
